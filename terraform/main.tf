27c1c261113a6bdb39827eba7b7ff69026062a0e
Kevin Zheng <k.zheng7077@gmail.com>
2026-04-06 18:51:42 -0400

    Added comments for readability

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,39 @@
+#########################
+# Terraform Configuration
+#########################
+
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
   }
 }
+
+########################
+# Provider Configuration
+########################
 
 provider "aws" {
   region = var.aws_region
 }
 
+###########
+# Random ID
+###########
+
 resource "random_id" "suffix" {
   byte_length = 4
 }
 
+############
+# S3 Buckets
+############
 resource "aws_s3_bucket" "misconfigured_bucket" {
   bucket = "misconfigured-bucket-${random_id.suffix.hex}"
 }
@@ -22,6 +41,10 @@ resource "aws_s3_bucket" "misconfigured_bucket" {
 resource "aws_s3_bucket" "secure_bucket" {
   bucket = "secure-bucket-${random_id.suffix.hex}"
 }
+
+####################################
+# S3 Bucket Encryption Configuration
+####################################
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "secure_encryption" {
   bucket = aws_s3_bucket.secure_bucket.id
@@ -31,6 +54,10 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "secure_encryption
     }
   }
 }
+
+#######################################
+# S3 Bucket Public Access Configuration
+#######################################
 
 resource "aws_s3_bucket_public_access_block" "s3_public_access" {
   bucket                  = aws_s3_bucket.misconfigured_bucket.id
@@ -47,6 +74,10 @@ resource "aws_s3_bucket_public_access_block" "s3_secure_access" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+##############
+# IAM Policies
+##############
 
 resource "aws_iam_policy" "misconfigured_iam_policy" {
   name        = "misconfigured-iam-policy"
